24e13711244f3bf9c28eee64be8cbf6943240e32
Ved Prakash <vedprakashsigh@gmail.com>
2023-12-16 13:33:26 +0530

    Basic Setup complete

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="bg-red-500 h-full">{children}</div>;
+  return <div>{children}</div>;
 };
 
 export default AuthLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,9 @@ import "./globals.css";
 const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Team Chat Application",
-  description: "Team Chat Application",
+  title: "UnityTalks",
+  description:
+    "Welcome to UnityTalks, the ultimate nexus for unified conversations and collaboration. Designed as an inclusive and versatile platform, UnityTalks brings people together, transcending boundaries through seamless communication channels. Engage in vibrant discussions, share ideas, and forge connections effortlessly within this dynamic space. From lively voice chats to organized text channels and comprehensive community tools, UnityTalks empowers individuals and groups to convene, collaborate, and build thriving communities. Experience the power of unity in communication, where every conversation leads to new connections and boundless opportunities.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
