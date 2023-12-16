2b8b0060269a3fa065a90cc24e6ba8cb45699244
Ved Prakash <vedprakashsigh@gmail.com>
2023-12-16 14:17:34 +0530

    Dark-Light Modes Added

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
-      This is a protected Route.
+      <ModeToggle />
     </div>
   );
 }
