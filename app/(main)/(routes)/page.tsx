eabb78a1f7dffbfdd1d32a4e70c9a0c750565359
Ved Prakash <vedprakashsigh@gmail.com>
2023-12-16 13:59:37 +0530

    Authentication Done using Clerk.com

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <div>This is a protected Route.</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+      This is a protected Route.
+    </div>
+  );
 }
