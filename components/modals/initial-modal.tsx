74b146a51e127d9e87d2887005f9f67cdc8a5924
Ved Prakash <vedprakashsigh@gmail.com>
2023-12-20 12:19:25 +0530

    Added a Landing Page to Create a Server

---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+} from "@/components/ui/dialog";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+
+const formScheme = z.object({
+	name: z.string().min(1, {
+		message: "Server Name is Required",
+	}),
+	imageUrl: z.string().min(1, {
+		message: "Image URL is Required",
+	}),
+});
+
+const InitialModal = () => {
+	const [isMounted, setIsMounted] = useState(false);
+
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
+
+	const form = useForm({
+		resolver: zodResolver(formScheme),
+		defaultValues: {
+			name: "",
+			imageUrl: "",
+		},
+	});
+
+	const isLoading = form.formState.isSubmitting;
+
+	const onSubmit = async (values: z.infer<typeof formScheme>) => {
+		console.log(values);
+	};
+
+	if (!isMounted) {
+		return null;
+	}
+
+	return (
+		<Dialog open>
+			<DialogContent className='bg-white text-black p-0 overflow-hidden'>
+				<DialogHeader className='pt-8 px-6'>
+					<DialogTitle className='text-2xl text-center font-bold'>Customize your Server</DialogTitle>
+					<DialogDescription className='text-center text-zinc-500'>
+						Give your Server a Personality with a name and an image. You can always cahnge it later.
+					</DialogDescription>
+				</DialogHeader>
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+						<div className='space-y-8 px-6'>
+							<div className='flex items-center justify-center text-center'>TODO: Image Upload</div>
+
+							<FormField
+								control={form.control}
+								name='name'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel className='uppercase text-xs font-bold to-zinc-500 dark:text-secondary/70'>
+											Server Name
+										</FormLabel>
+										<FormControl>
+											<Input
+												disabled={isLoading}
+												className='bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0'
+												placeholder='Enter Server Name'
+												{...field}
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+						</div>
+						<DialogFooter className='bg-gray-100 px-6 py-4'>
+							<Button disabled={isLoading} variant='primary'>
+								Create
+							</Button>
+						</DialogFooter>
+					</form>
+				</Form>
+			</DialogContent>
+		</Dialog>
+	);
+};
+
+export default InitialModal;
